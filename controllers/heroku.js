beddaf4d7fdf74e24ac697fac14d2672c53e7e75
Flashminat0 <wyasaslowe@gmail.com>
2022-04-07 17:15:15 +0530

    Checking auto deploy

--- controllers/heroku.js
+++ controllers/heroku.js
@@ -3,3 +3,9 @@ export const herokuFunc = (req,res) => {
 
   res.send(`Hello ${hello}`);
 }
+
+
+export const helloTest = (req,res) => {
+
+  res.send(`Hello this is a test`);
+}
--- routes/heroku.js
+++ routes/heroku.js
@@ -1,8 +1,9 @@
 import express from "express";
-import {herokuFunc} from "../controllers/heroku";
+import {herokuFunc , helloTest} from "../controllers/heroku";
 
 const router = express.Router();
 
 router.get("/heroku", herokuFunc);
+router.get("/", helloTest);
 
 module.exports = router;
