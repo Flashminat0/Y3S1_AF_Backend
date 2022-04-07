6c284008fb76bff24f767fa5666f4d27ad3e5354
Flashminat0 <wyasaslowe@gmail.com>
2022-04-07 16:33:20 +0530

    Heroku Support

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 import express from "express";
 import {readdirSync} from "fs";
-const cors=require("cors");
+
+const cors = require("cors");
 const app = express();
 
 app.use(express.json({limit: "5mb"}));
 
-const corsOptions ={
-    origin:'*',
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
 }
 
 app.use(cors(corsOptions)) // Use this after the variable declaration
