4956f6ede3de0fc70718dea8db324a8d3d11e47d
Sameera <sameeranicho@gmail.com>
2022-05-22 10:59:37 +0530

    userModalCreate

---
+++ controllers/users.js
@@ -0,0 +1,12 @@
+import User from "../models/user";
+
+export const getUsers = async (req,res) => {
+
+    try {
+        const result = await User.find();
+        res.json({result});
+    } catch (error) {
+        res.status(500).json({message: 'Something went wrong'});
+    }
+  }
+  
