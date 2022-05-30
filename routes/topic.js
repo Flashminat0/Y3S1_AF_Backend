e3b7abf4d267c3067cccb460bcdae4ee698b3963
Tharushi1212 <80015599+Tharushi1212@users.noreply.github.com>
2022-05-30 17:09:39 +0530

    backendchanges

--- routes/topic.js
+++ routes/topic.js
@@ -4,7 +4,6 @@ import {inputTopic, getTopic, deleteTopic} from '../controllers/topic'
 const router = express.Router()
 
 router.post('/input-topic', inputTopic)
-
 router.get('/display-topic', getTopic)
 router.delete('/removetopic', deleteTopic)
 
