2bcd3a08cf34189fdc04fc3b4c7a98585b565122
Flashminat0 <wyasaslowe@gmail.com>
2022-06-04 15:34:45 +0530

    file upload side fix on supervisor

---
+++ controllers/submittedTopic.js
@@ -0,0 +1,3 @@
+export const test111 = async (req, res) => {
+
+}
--- routes/messages.js
+++ routes/messages.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import {approveProject, fetchMessages, sendMessage} from '../controllers/messages'
+import {approveProject, fetchMessages, rejectProject, sendMessage} from '../controllers/messages'
 
 const router = express.Router()
 
 router.post('/chat/send-message', sendMessage)
 router.post('/chat/fetch-message', fetchMessages)
 router.post('/chat/approve-project', approveProject)
+router.post('/chat/reject-project', rejectProject)
 
 module.exports = router
